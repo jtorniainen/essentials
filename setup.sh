987df5643a8b6aa1765757f7f944fcfd1da39e64
Jari <jtorniainen@users.noreply.github.com>
2016-05-03 10:06:57 +0300

    added neovim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Install basic python stuff
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+# Install vim
 sudo apt-get install vim
 
+# Install neovim
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# Copy .vimrc
 cp ./.vimrc ~/
+
+# TODO: Copy nvim configurations
