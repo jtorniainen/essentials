c83acd9be9974b219aa097f67e738abbb4d25b52
Jari <jtorniainen@users.noreply.github.com>
2016-05-03 10:10:51 +0300

    added zsh and ohmyzsh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Install basic python stuff
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 
+# Install zsh and oh-my-zsh
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 # Install vim
 sudo apt-get install vim
 
